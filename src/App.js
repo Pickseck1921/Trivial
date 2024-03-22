e869e7b07f78ad8c4593723d5236f0b96820fde8
unknown <valeria.montes.canga@gmail.com>
2024-03-22 16:26:07 +0200

    Message

--- src/App.js
+++ src/App.js
@@ -10,22 +10,25 @@ let App = () => {
   let [questions, setQuestions] = useState ([
     {
       text : "¿Quién es la mejor actriz del mundo?",
-      options: ["Chelo", "Marie Curie", "Meryl Streep"]
+      options: ["Chelo", "Marie Curie", "Meryl Streep"],
+      answer : 2
     },
     {
       text : "¿Cómo se llama la hija de Tania y Josh?",
-      options: ["Eloise", "Viola", "Agustina"]
+      options: ["Eloise", "Viola", "Agustina"],
+      answer : 1
     },
     {
       text : "¿En qué año se fundó Pickseck?",
-      options: ["1100", "2018", "1921"]
+      options: ["1100", "2018", "1921"],
+      answer : 2
     },
 
   ])
   return (
     <div className="App">
      <PlayerInfoComponent points= {points}/>
-     <QuestionComponent questions={questions} />
+     <QuestionComponent questions={questions} points= {points} setPoints={setPoints} />
         
     </div>
   );
--- src/Components/QuestionsComponent.js
+++ src/Components/QuestionsComponent.js
@@ -2,18 +2,52 @@ import { useState } from "react";
 
 let QuestionComponent = (props) =>{
 
-        let {questions} = props;
+        let {questions, setPoints, points} = props;
         let [numberOfQuestions, setNumberOfQuestions] = useState(0)
+        let [message, setMessage] = useState(null)
 
+
+        let answerQuestion = (answer) =>{
+            if (answer == questions[numberOfQuestions].answer) {
+                setMessage ("Right answer")
+                setPoints(points+1)
+            } else {
+                setMessage ("Wrong answer")
+            }
+
+            if(numberOfQuestions < questions.length-1){
+                setNumberOfQuestions(numberOfQuestions+1)
+                setTimeout(()=>{
+                        setMessage (null)
+                }, 2000)
+            } else{
+
+                setTimeout(()=> {
+                    setMessage("Finish")
+                },4000)
+
+
+                setTimeout(()=> {
+                    setPoints(0)
+                    setNumberOfQuestions(0)
+                },4000)
+            }
+        }
         return (
             <div className="question">
+                {message != null &&
+                <h2>{message}</h2>
+                    }
+
+
                 <div>
                     <h2>Question</h2>
                     <p id="questionText">{questions[numberOfQuestions].text}</p>
                     <ul class="options">
-                        <li><button> {questions[numberOfQuestions].options[0]}</button></li>
-                        <li><button class="buttom"> {questions[numberOfQuestions].options[1]}</button></li>
-                        <li><button class="buttom"> {questions[numberOfQuestions].options[2]}</button></li>
+                        {questions[numberOfQuestions].options.map((option, i) =>
+                           <li><button onClick={()=>{answerQuestion(i)}}> {option}</button></li> 
+                        )}
+                        
                     </ul>
                 </div>
 
